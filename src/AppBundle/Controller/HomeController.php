9972c740e9d3013e9f894050dde175db956a6da9
Anh Nguyen <anhnt0212@gmail.com>
2016-11-25 14:29:42 +0700

    fix css

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -4,10 +4,10 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->render('AppBundle:Home:index.html.twig');
     }
 }
