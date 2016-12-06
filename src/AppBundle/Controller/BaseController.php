2feb0da4252b34db435e6f701f21fdc8f119a60e
Anh Nguyen <anhnt0212@gmail.com>
2016-12-06 18:09:45 +0700

    fix

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -22,9 +22,23 @@ class BaseController extends Controller
         $em = $this->getDoctrine()->getManager();
         $qb = $em->getRepository('AppBundle:Category')->createQueryBuilder('c');
         $categories = $qb->where('c.enabled = 1')->orderBy('c.name', 'ASC')->getQuery()->getResult();
+        $categories = $this->buildTree($categories);
         $variables = array(
             'categories' => $categories
         );
         return $this->render('AppBundle:Block:menu.html.twig', $variables);
+    }
+
+    public function buildTree(array $elements, $parentId = 0, $parent_id_field = 'parent_id') {
+        $branch = array();
+        foreach ($elements as $element) {
+            if ($element[$parent_id_field] == $parentId) {
+                $children = buildTree($elements, $element['id']);
+                $element['children'] = empty($children) ? [] : $children;
+
+                $branch[] = $element;
+            }
+        }
+        return $branch;
     }
 }
