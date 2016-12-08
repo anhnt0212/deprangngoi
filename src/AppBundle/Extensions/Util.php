81ddd862558e9d414f818ffad9c5961efb0f882d
Anh Nguyen <anhnt0212@gmail.com>
2016-12-08 13:32:46 +0700

    custom core

--- src/AppBundle/Extensions/Util.php
+++ src/AppBundle/Extensions/Util.php
@@ -74,5 +74,15 @@ class Util {
         // done!
         return $string;
     }
+    public static function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 
 }
