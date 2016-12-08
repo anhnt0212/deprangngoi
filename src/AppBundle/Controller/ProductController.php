4613f4573489813b7d6be218fafad2fd7c0300a5
Anh Nguyen <anhnt0212@gmail.com>
2016-12-08 23:17:11 +0700

    fix bug

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,10 +9,49 @@ class ProductController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $this->data['title'] = 'Mỹ phẩm đẹp rạng ngời';
-        $this->data['item']['alias'] = 'muc-san-pham';
-        $this->data['product']['alias'] = 'chi-tiet-san-pham';
+        $data = \AppBundle\Controller\BaseController::setMetaData();
         $alias = $request->get('slug', NULL);
-        return $this->render('AppBundle:Product:index.html.twig',$this->data);
+        $manager = $this->getDoctrine()->getManager();
+        $product = $manager->getRepository('AppBundle:Product')->findOneBy(array('alias' => trim($alias)));
+        if ($product->getOld() == 1) {
+            $sql = "SELECT * FROM product_gallery WHERE product_gallery.productId =" . $product->getId();
+            $em = $this->getDoctrine()->getManager();
+            $stmt = $em->getConnection()->prepare($sql);
+            $stmt->execute();
+            $oldGallery = $stmt->fetchAll();
+        } else {
+            $oldGallery = null;
+        }
+        $data['item'] = $product;
+        $data['oldGallery'] = $oldGallery;
+        $category = $manager->getRepository('AppBundle:Category')
+            ->createQueryBuilder('p')
+            ->join('p.products', 'c')
+            ->where("c.id = " .$product->getId())
+            ->getQuery()->getArrayResult();
+        $productLike = $manager->getRepository('AppBundle:Product')
+            ->createQueryBuilder('p')
+            ->join('p.categories', 'c')
+            ->where("c.id = " .$category[0]['id'])
+            ->getQuery()->setMaxResults(6)->getArrayResult();
+        $data['productLike'] = $productLike;
+        return $this->render('AppBundle:Product:detail.html.twig', $data);
+    }
+    public function searchAction(Request $request)
+    {
+        $slug = 'new-today';
+        $data = \AppBundle\Controller\BaseController::setMetaData();
+        $manager = $this->getDoctrine()->getManager();
+        $category = $manager->getRepository('AppBundle:Category')->findOneBy(array('alias' => trim($slug)));
+        $product = $manager->getRepository('AppBundle:Product')
+            ->createQueryBuilder('p')
+            ->join('p.categories', 'c')
+            ->where("c.id = " .$category->getId())
+            ->getQuery()->getArrayResult();
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($product, $request->query->getInt('page', 1), 15);
+        $data['items'] = $pagination;
+        $data['category'] = $category;
+        return $this->render('AppBundle:Product:search.html.twig', $data);
     }
 }
