66b2cfba092a0ec03107d5361a28a166d6ca718b
Anh Nguyen <anhnt0212@gmail.com>
2016-12-23 10:49:55 +0700

    show contact info

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -9,6 +10,13 @@ class ContactController extends Controller
     public function indexAction()
     {
         $data = \AppBundle\Controller\BaseController::setMetaData();
-        return $this->render('AppBundle:Contact:index.html.twig',$data);
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('AppBundle:Config')->createQueryBuilder('ad');
+        $footer = $qb->getQuery()->setMaxResults(2)->getResult();
+        if ($footer) {
+            $data['contact'] = $footer[0];
+            $data['facebook'] = $footer[1];
+        }
+        return $this->render('AppBundle:Contact:index.html.twig', $data);
     }
 }
